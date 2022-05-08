213068f133ca015a61816f590c2764d9ceffc1c6
Thomas Hayashi <thomashsh11@gmail.com>
2022-05-08 20:58:31 -0300

    test: verified behavior when modifying an existing exchange rate

--- JS/test_money.js
+++ JS/test_money.js
@@ -4,11 +4,12 @@ import { Portfolio } from './portfolio.js'
 import { Bank } from './bank.js'
 
 class MoneyTest {
-    constructor() {
+    setUp() {
         this.bank = new Bank();
         this.bank.addExchangeRate("EUR", "USD", 1.2);
         this.bank.addExchangeRate("USD", "KRW", 1100);
     }
+
     testMultiplication() {
         let tenEuros = new Money(10, "EUR");
         let twentyEuros = new Money(20, "EUR");
@@ -58,26 +59,27 @@ class MoneyTest {
         assert.throws(() => portfolio.evaluate(this.bank, "Kalganid"), expectedError);
     }
 
-    testConversion() {
-        let bank = new Bank();
-        bank.addExchangeRate("EUR", "USD", 1.2);
+    testConversionWithDifferentRatesBetweenTwoCurrencies() {
         let tenEuros = new Money(10, "EUR");
-        assert.deepStrictEqual(bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+        assert.deepStrictEqual(this.bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+
+        this.bank.addExchangeRate("EUR", "USD", 1.3);
+        assert.deepStrictEqual(this.bank.convert(tenEuros, "USD"), new Money(13, "USD"));
     }
 
     testConversionWithMissingExchangeRate() {
-        let bank = new Bank();
         let tenEuros = new Money(10, "EUR");
         let expectedError = new Error("EUR->Kalganid");
-        assert.throws(function() {bank.convert(tenEuros, "Kalganid")}, expectedError);
+        assert.throws(() => this.bank.convert(tenEuros, "Kalganid"), expectedError);
     }
 
     runAllTests() {
-        let testMethods = this.getAllTestMethods();
+        let testMethods = this.randomizeTestOrder(this.getAllTestMethods());
         testMethods.forEach(m => {
             console.log("Running: %s()", m);
             let method = Reflect.get(this, m);
             try {
+                this.setUp();
                 Reflect.apply(method, this, []);
             } catch (e) {
                 if( e instanceof assert.AssertionError)
@@ -86,6 +88,14 @@ class MoneyTest {
                     throw e;
             }
         });
+    }
+
+    randomizeTestOrder(testMethods) {
+        for(let i = testMethods.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+        }
+        return testMethods;
     }
 
     getAllTestMethods() {
