2ae2bc472311f58bc87fceaded58f592c5adcdc5
Thomas Hayashi <thomashsh11@gmail.com>
2022-05-14 21:23:08 -0300

    feat: Currency and money refactor

--- JAVA/src/com/thomashayashi/Money.java
+++ JAVA/src/com/thomashayashi/Money.java
@@ -2,11 +2,24 @@ package com.thomashayashi;
 
 public abstract class Money {
     protected int amount;
+    protected  String currency;
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
-    public static Money franc(int amount) { return new Franc(amount); }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    public String currency() {
+        return currency;
+    }
+
     abstract Money times(int multiplier);
 
     @Override
